43fc85288040ec29b53d106cd08a5f9be188d451
gabi droid <193822798+gabriel-droidel@users.noreply.github.com>
2025-01-28 17:08:43 +0100

    Add comments for clearer code

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
+// object constructor for the book
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
@@ -8,21 +9,24 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
+    //creates a book object and adds it to the array
 	const book = new Book(title, author, pages, read);
 	myLibrary.push(book);
 }
 
 console.log(myLibrary);
 
+// handle selectors from html
 const shelf = document.querySelector('.book-shelf');
 const bookDisplayed = document.createElement('div');
+shelf.appendChild(bookDisplayed);
 const addBook = document.querySelector('.addBook');
 const dialog = document.querySelector('#dialog');
 const submitBook = document.querySelector('#submit');
 const closeDialog = document.querySelector('#btn-close');
 
-addBook.addEventListener('click', () => dialog.showModal());
-submitBook.addEventListener('click', (e) => {
+addBook.addEventListener('click', () => dialog.showModal()); // pops up modal form to add a book
+submitBook.addEventListener('click', (e) => { // submit button from the form to add and display the book on the page
 	e.preventDefault();
 	const titleInput = document.querySelector('#title');
 	const authorInput = document.querySelector('#author');
@@ -33,16 +37,16 @@ submitBook.addEventListener('click', (e) => {
 		pagesInput.value,
 		'yes'
 	);
-    displayBook(myLibrary);
-    dialog.close();
+	displayBook(myLibrary);
+	dialog.close();
 });
-closeDialog.addEventListener('click', (e) => {
+closeDialog.addEventListener('click', (e) => { //close the modal
 	e.preventDefault();
 	dialog.close();
 });
-shelf.appendChild(bookDisplayed);
 
 function displayBook(myLibrary) {
+    // create a div with each book in the array
 	bookDisplayed.textContent = '';
 	myLibrary.forEach((item) => {
 		const bookTitle = document.createElement('p');
