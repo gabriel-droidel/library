44b2d3055f1cb5a5f86198aa0abc264d947e6581
gabi droid <193822798+gabriel-droidel@users.noreply.github.com>
2025-01-29 12:01:09 +0100

    Structure js code and add form validation

--- code.js
+++ code.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+// Object related //
+
 function Book(title, author, pages, read) {
 	// object constructor for the book
 	this.title = title;
@@ -9,26 +11,36 @@ function Book(title, author, pages, read) {
 	this.index = null;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-	//creates a book object and adds it to the array
-	const book = new Book(title, author, pages, read);
-	myLibrary.push(book);
-}
+Book.prototype.toggleRead = function () {
+	// toggle read status on the book object
+	this.read = !this.read;
+};
 
-console.log(myLibrary);
+// DOM Container Elements //
 
-// handle selectors from html
-const shelf = document.querySelector('.book-shelf');
-const bookDisplayed = document.createElement('div');
-bookDisplayed.classList.add('book-list');
-shelf.appendChild(bookDisplayed);
+const booksContainer = document.querySelector('.books-container');
+const booksDisplayBox = document.createElement('div');
+booksDisplayBox.classList.add('book-list');
+booksContainer.appendChild(booksDisplayBox);
+
+// DOM Dialog Elements //
+
 const addBook = document.querySelector('.addBook');
 const dialog = document.querySelector('#dialog');
-const submitBook = document.querySelector('#submit');
+const buttonSubmit = document.querySelector('#submit');
 const closeDialog = document.querySelector('#btn-close');
 
+// DOM Dialog Buttons //
+
 addBook.addEventListener('click', () => dialog.showModal()); // pops up modal form to add a book
-submitBook.addEventListener('click', (e) => {
+buttonSubmit.addEventListener('click', (e) => submitBook(e));
+closeDialog.addEventListener('click', (e) => {
+	//close the modal
+	e.preventDefault();
+	dialog.close();
+});
+
+function submitBook(e) {
 	// submit button from the form to add and display the book on the page
 	e.preventDefault();
 	const titleInput = document.querySelector('#title');
@@ -42,56 +54,72 @@ submitBook.addEventListener('click', (e) => {
 		pagesInput.value,
 		readInput.checked
 	);
-	displayBook(myLibrary);
+	displayBooks(myLibrary);
 	dialog.close();
-});
-closeDialog.addEventListener('click', (e) => {
-	//close the modal
-	e.preventDefault();
-	dialog.close();
-});
+}
 
-function displayBook(myLibrary) {
-	// create a div with each book in the array
-	bookDisplayed.textContent = '';
+// Functions //
+
+function addBookToLibrary(title, author, pages, read) {
+	//creates a book object and adds it to the array
+	const book = new Book(title, author, pages, read);
+	myLibrary.push(book);
+}
+
+function displayBooks(myLibrary) {
+	// display each individual book stores inside the array
+	booksDisplayBox.textContent = ''; // clear the display
 	myLibrary.forEach((item, index) => {
-		item.index = index;
-		const bookInfoDisplay = document.createElement('div');
-		bookInfoDisplay.classList.add('book-style');
-		['title', 'author', 'pages'].forEach((element) => {
-			const p = document.createElement('p');
-			p.textContent =
-				capitalizeWord([element].toString()) + ` : ` + item[element];
-			bookInfoDisplay.appendChild(p);
-		});
-		const readStatus = document.createElement('input');
-		readStatus.type = 'checkbox';
-		readStatus.id = `${item.index}`;
-		readStatus.addEventListener('click',()=>item.toggleRead());
-		readStatus.checked = item.read;
-		bookInfoDisplay.appendChild(readStatus);
-		const removeBookButton = document.createElement('button');
-		removeBookButton.textContent = 'Delete';
-		removeBookButton.classList.add('remove-btn');
-		removeBookButton.addEventListener('click', () =>
-			removeBook(myLibrary, item.index)
-		);
-		bookInfoDisplay.appendChild(removeBookButton);
-		bookDisplayed.appendChild(bookInfoDisplay);
+		item.index = index; // assign an index to each book for keeping track of books in the DOM
+		const bookDisplayed = document.createElement('div'); // container for all book data
+		bookDisplayed.classList.add('book-style');
+		displayBookContent(bookDisplayed, item); // displays all the information from the form in the modal dialog
+		handleReadStatus(bookDisplayed, item); // handles the logic of the read checkbox for each book
+		createRemoveButton(bookDisplayed, item); // adds a button to delete the book
+		booksDisplayBox.appendChild(bookDisplayed);
 	});
 }
 
 function capitalizeWord(string) {
+	// get a string and return it capitalized
 	const array = string.split('');
 	array[0] = array[0].toUpperCase();
 	return array.join('');
 }
 
 function removeBook(array, index) {
+	// remove the book from the array considering the index
 	array.splice(index, 1);
-	displayBook(myLibrary);
+	displayBooks(myLibrary);
 }
 
-Book.prototype.toggleRead = function () {
-	this.read = !this.read;
-};
+// Display Book Functions //
+
+function displayBookContent(container, item) {
+	['title', 'author', 'pages'].forEach((element) => {
+		const p = document.createElement('p');
+		p.textContent =
+			capitalizeWord([element].toString()) + ` : ` + item[element];
+		container.appendChild(p);
+	});
+}
+
+function handleReadStatus(container, item) {
+	const readStatus = document.createElement('input');
+	readStatus.type = 'checkbox';
+	readStatus.id = `${item.index}`;
+	readStatus.addEventListener('click', () => item.toggleRead());
+	readStatus.checked = item.read;
+	container.appendChild(readStatus);
+}
+
+function createRemoveButton(container, item) {
+	const removeBookButton = document.createElement('button');
+	removeBookButton.textContent = 'Delete';
+	removeBookButton.classList.add('remove-btn');
+	removeBookButton.addEventListener('click', () =>
+		removeBook(myLibrary, item.index)
+	);
+
+	container.appendChild(removeBookButton);
+}
